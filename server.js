0c210e4c39781dcc9bd2809cef81bfdbe17249cc
Emily Rowley <erowley49@gmail.com>
2023-05-20 12:45:19 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+/* ******************************************
+ * This server.js file is the primary file of the 
+ * application. It is used to control the project.
+ *******************************************/
+/* ***********************
+ * Require Statements
+ *************************/
+const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+const env = require("dotenv").config()
+const app = express()
+const baseController = require("./controllers/baseController")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
+/* ***********************
+ * Routes
+ *************************/
+app.use(require("./routes/static"))
+// Index route
+app.get("/", baseController.buildHome)
+// Inventory routes
+app.use("/inv", require("./routes/inventoryRoute"))
+
+/* ***********************
+ * Local Server Information
+ * Values from .env (environment) file
+ *************************/
+const port = process.env.PORT
+const host = process.env.HOST
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
+
+
+/* DEBUGGING EXAMPLE */
+// function square(num1){
+//   let result = 0;
+//   result = num1 * num1;
+//   return result;
+// }
+
+// const num1 = 5;
+// console.log(square(num1));
